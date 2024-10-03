28b0557d8587585257c0a780d93bbb8bdeb784b4
ssshar-014 <ssharathsurya@gmail.com>
2024-10-04 01:55:12 +1000

    feat(sql-integration): setup sql integration

---
+++ src/db.ts
@@ -0,0 +1,19 @@
+import knex from 'knex';
+
+// Initialize Knex connection to SQL Server
+const db = knex({
+  client: 'mssql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'sa',
+    password: 'P@ssw0rd1',
+    database: 'tempdb',
+    options: {
+      encrypt: false,
+      trustServerCertificate: true,
+      trustedConnection: true
+    },
+  },
+});
+
+export default db;
