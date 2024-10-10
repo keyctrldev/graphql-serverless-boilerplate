15141962f06fe893ff8a00dc02a5841b34741b9c
Sharath Soma <140714683+ssshar-014@users.noreply.github.com>
2024-10-10 23:24:26 +1100

    Feat/dynamo enhancements (#5)
    * upgrade awssdk from v2 to v3
    
    * update packages
    
    * configure env variables for sql

--- src/dynamodb.ts
+++ src/dynamodb.ts
@@ -1,10 +1,12 @@
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
 
-export const dynamoDB = new DynamoDB.DocumentClient({
-  region: "localhost",
-  endpoint: "http://localhost:8000",
+export const dynamoDB = DynamoDBDocument.from(new DynamoDB({
+  // region: "us-east-1",
+  // endpoint: "http://localhost:8000",
+  // endpoint: "dynamodb.us-east-1.amazonaws.com"
   // This just hardcoded for quick testing
   // TODO: this will be moved to secret store later
-  accessKeyId: "myAccessKeyId", // Optional if using IAM roles or default credentials
-  secretAccessKey: "mySecretAccessKey", // Optional
-});
+  // accessKeyId: "myAccessKeyId", // Optional if using IAM roles or default credentials
+  // secretAccessKey: "mySecretAccessKey", // Optional
+}));
