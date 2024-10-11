cc85e165fe17c05fd83c7f56f6785cb127ef69c2
suryatejavarma <suryateja.varma777@gmail.com>
2024-10-11 10:30:46 +0530

    Get secrets from secret manager

---
+++ src/secrets.ts
@@ -0,0 +1,21 @@
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+export const getSecret: any = async () => {
+  try {
+    const params = {
+      SecretId: `arn:aws:secretsmanager:us-east-1:575892549481:secret:MySecrtes-2kMVRi`,
+    };
+    const command = new GetSecretValueCommand(params);
+    const response = await client.send(command);
+    return response.SecretString;
+  } catch (error) {
+    return error;
+  }
+}
