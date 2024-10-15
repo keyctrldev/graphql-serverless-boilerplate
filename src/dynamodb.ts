f06171f9f39aa8827bbd718ed90bc5ff95af74b0
suryatejavarma <suryateja.varma777@gmail.com>
2024-10-15 15:42:09 +0530

    AWS secrets

--- src/dynamodb.ts
+++ src/dynamodb.ts
@@ -1,10 +1,9 @@
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { getSecret } from "./secrets";
 
 export const dynamoDB = DynamoDBDocument.from(new DynamoDB({
   region: "us-east-1",
-  endpoint: getSecret.Dynamodb,
+  endpoint: "http://localhost:8000",
   // endpoint: "dynamodb.us-east-1.amazonaws.com"
   // This just hardcoded for quick testing
   // TODO: this will be moved to secret store later
