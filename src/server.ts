8b4b30cba39a2c497650a4e9f005de61d1d3750b
suryatejavarma <suryateja.varma777@gmail.com>
2024-10-17 08:16:19 +0530

    constraint_directives

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,29 @@ import claimResolver from './resolver/claims.resolver';
 import { userResolver } from './resolver/users.resolver';
 import { readFileSync } from 'fs';
 import { join } from 'path';
+import { createApolloQueryValidationPlugin, constraintDirectiveTypeDefs } from 'graphql-constraint-directive'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+
 
 // Read the GraphQL schema
 const typeDefs = readFileSync(join(__dirname, '/schema.graphql'), 'utf-8');
 
+let schema = makeExecutableSchema({
+  typeDefs: [constraintDirectiveTypeDefs, typeDefs]
+})
+
+const plugins = [
+  createApolloQueryValidationPlugin({
+    schema
+  })
+]
+
 // Apollo Server instance creation
 export const server = new ApolloServer({
-  typeDefs,
+  schema,
+  plugins,
   resolvers: [claimResolver, userResolver],
+  // schemaTransforms: [constraintDirective()],
   introspection: process.env.NODE_ENV !== 'production',
 //   playground: process.env.NODE_ENV !== 'production',
 });
